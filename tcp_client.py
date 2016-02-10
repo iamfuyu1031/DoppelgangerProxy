e1fce0ba92ba8567134313267bce49f5f3241544
iamfuyu1031 <fu2@g.clemson.edu>
2016-02-10 16:21:52 -0500

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -9,11 +9,10 @@ import socket
 host = 'localhost' 
 port = 10000 
 size = 1024 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port)) 
 while 1:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	s.connect((host,port)) 
 	s.send('hello world') 
 	time.sleep(0.5)
 	data = s.recv(size) 
-	s.close() 
 	print 'Received:', data
