59e8e04cc1773aaf944719c4940856a60d5659c4
iamfuyu1031 <fu2@g.clemson.edu>
2016-02-10 16:22:11 -0500

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -5,18 +5,19 @@ A simple echo server
 """ 
 
 import socket 
+import time
 
 host = '' 
 port = 50000 
 backlog = 5 
 size = 1024 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host,port)) 
 s.listen(backlog) 
-while 1: 
-    client, address = s.accept() 
+client, address = s.accept() 
+while 1:
     data = client.recv(size) 
     if data: 
+        print 'received data: ' + data
         client.send(data) 
-    client.close()
-    
